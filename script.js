b59543fd3496bae736ff2e234f5decca0fb8d688
Caleb Tseng <calebtseng07@gmail.com>
2022-10-21 04:22:30 -0700

    add function to black button and eraser button

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const defaultGrid = 16;
-const defaultColor = 'black';
+let defaultColor = '#363636';
 
 
 //grid-box
@@ -7,13 +7,13 @@ const gridBox = document.querySelector("#grid");
 
 
 //color selectors
-const color = document.querySelector("#color-picker");
+const color = document.querySelector(".color-picker");
 const black = document.querySelector("#black");
 const rainbow = document.querySelector("#rainbow");
 
 //reset
 const eraser = document.querySelector("#eraser");
-const reset = document.querySelector("#clear");
+const reset = document.querySelector("#clear")
 
 
 //grid set up
@@ -24,26 +24,37 @@ function gridSetUp(size) {
         grid.style.backgroundColor = 'white';
         gridBox.appendChild(grid);
     }
-    gridBox.style.gridTemplateColumns = `repeat(16, auto)`;
-    gridBox.style.gridTemplateRows = `repeat(16, auto)`;
+    console.log(size);
+    gridBox.style.gridTemplateColumns = `repeat(${size}, auto)`;
+    gridBox.style.gridTemplateRows = `repeat(${size}, auto)`;
 }
 
 gridSetUp(defaultGrid)
 
 //default color pick
-grid.addEventListener("mousedown", (event) => {
-    event.target.style.backgroundColor = "#363636";
-});
-// grid.addEventListener("mouseover", (event) => {
-//     event.target.style.backgroundColor = "#363636";
+// grid.addEventListener("mousedown", (event) => {
+//     event.target.style.backgroundColor = "defaultColor";
 // });
+grid.addEventListener("mouseover", (event) => {
+    event.target.style.backgroundColor = defaultColor;
+});
 
+//default color
+black.addEventListener("click", () => {
+    defaultColor = "#363636"
+});
 
+//eraser button
+eraser.addEventListener("click", () => {
+    defaultColor = "white"
+});
+
+//color-picker button
+color.addEventListener("click", () => {
+    defaultColor = ""
+});
 //clear button
 function refreshPage() {
     window.location.reload(true);
 }
 reset.addEventListener('click', refreshPage);
-
-
-// console.log(gridBox);

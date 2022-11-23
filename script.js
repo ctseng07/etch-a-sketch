7ce1911a0bebfb5ab0f9e57c64d8ba5d251ec7a5
Caleb Tseng <calebtseng07@gmail.com>
2022-11-23 10:43:05 -0800

    add function to rainbow button

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-const rows = 16;
+let defaultSize = 16;
 let defaultColor = '#363636';
 let defaultMode = 'color';
+let state = "";
 
 /////////////////////////////////////////////////////////////
 //*Query Selectors*//
 //Grid Box
 const gridBox = document.querySelector("#grid");
-let slider = document.querySelector("#sizeSlider")
-let output = document.querySelector("#sizeValue");
+let sizeSlider = document.querySelector("#sizeSlider")
+let sizeValue = document.querySelector("#sizeValue");
 
-/////////////////////////////////////////////////////////////
 //Color Selectors
 let colorPicker = document.querySelector("#colorPicker");
-const black = document.querySelector("#black");
-const rainbow = document.querySelector("#rainbow");
+let rainbow = document.querySelector("#rainbow");
 
 //Reset
 const eraser = document.querySelector("#eraser");
@@ -25,25 +24,39 @@ function gridSetUp(size) {
     for (let i = 0; i < size * size; i++) {
         let grid = document.createElement("div");
         grid.classList.add('square');
-        grid.style.backgroundColor = 'white';
+        grid.style.backgroundColor = "white";
         gridBox.appendChild(grid);
     }
+
     gridBox.style.gridTemplateColumns = `repeat(${size}, auto)`;
     gridBox.style.gridTemplateRows = `repeat(${size}, auto)`;
 };
+gridSetUp(defaultSize);
 
-gridSetUp(rows);
+//Grid Change
+function changeSize(value) {
+    defaultSize = value;
+    sizeSlider.addEventListener('input', (e) => {
+        updateSizeValue(e.target.value)
+    });
+    reloadGrid();
 
+};
+function reloadGrid() {
+    gridChange();
+    gridSetUp(defaultSize);
+};
 
-//color-picker button
-// function setCurrentColor(newColor) {
-//     currentColor = newColor
-// }
-// colorPicker.oninput = (e) => setCurrentColor(e.target.value);
-// console.log(newColor);
-// function updateSizeValue(value) {
-//     output.innerHTML = `${value} x ${value}`
-// }
+function gridChange() {
+    grid.textContent = '';
+}
+function updateSizeValue(value) {
+    sizeValue.textContent = `${value} x ${value}`
+}
+
+sizeSlider.addEventListener("change", (e) => {
+    changeSize(e.target.value);
+});
 
 /////////////////////////////////////////////////////////////
 //*Color Button Settings*//
@@ -52,85 +65,141 @@ grid.addEventListener("mousedown", (event) => {
     event.target.style.backgroundColor = defaultColor;
 });
 grid.addEventListener("mouseover", (event) => {
+    if (state == 'rainbow') {
+        defaultColor == colorChange()
+    } else if
+        (state == 'color') {
+        defaultColor == colorPicker
+    } else {
+        defaultColor == "3636336"
+    }
     event.target.style.backgroundColor = defaultColor;
 });
 
-//Color Picker*//
-
-//Black button
-black.addEventListener("click", () => {
-    defaultColor = "#363636"
+// Color Picker//
+colorPicker.addEventListener("input", (newColor) => {
+    defaultColor = newColor.target.value;
+    state = 'color'
 });
 
-//Rainbow Button
-rainbow.addEventListener("click", (e) => {
-    const r = Math.floor(Math.random() * 256)
-    const g = Math.floor(Math.random() * 256)
-    const b = Math.floor(Math.random() * 256)
-    defaultColor = `rgb(${r}, ${g}, ${b})`
+// //Rainbow Button
+function colorChange() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    defaultColor = `rgb(${r}, ${g}, ${b})`;
+}
+
+rainbow.addEventListener("click", () => {
+    state = 'rainbow';
 });
-//////////////////////////////////////////////////////
-//*Reset Button Settings*//
+
+
 //Eraser Button
 eraser.addEventListener("click", () => {
     defaultColor = "white"
 });
 
 //Clear Button
-const clearGrid = document.querySelectorAll('.square');
+reset.addEventListener('click', reloadGrid)
+let clearGrid = document.querySelectorAll('.square');
 reset.addEventListener('click', () => {
     clearGrid.forEach(element => {
         element.style.backgroundColor = "white";
     })
 });
-//////////////////////////////////////////////////////////////
-//*Slider Button*// figure out how change display from single digit to 16 x 16 
-// slider.onmousemove = (e) => updateSizeValue(e.target.value);
-// slider.onchange = (e) => changeSize(e.target.value);
+///////////////////////////////////////////////////////////////////
 
-// function changeSize(value) {
-//     setCurrentsize(value);
-//     updateSizeValue(value);
+// let defaultSize = 16;
+// let defaultColor = '#363636';
+// let defaultMode = 'color';
+
+// let currentSize = defaultSize;
+// let currentColor = defaultColor;
+// let currentMode = defaultMode;
+
+// function setCurrentColor(newColor) {
+//     currentColor = newColor
 // }
 
-// function updateSizeValue(value) {
-//     output.innerHTML = `${value} x ${value}`;
-
-output.innerHTML = slider.value;
-
-slider.oninput = function () {
-    output.innerHTML = slider.value;
-};
-
-//////////////////////////////////////////////////////////////
-//**not used **//
-// function randomColor() {
-//     let generateColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
-//     return generateColor;
+// function setCurrentMode(newMode) {
+//     // activateButton(newMode)
+//     currentMode = newMode
 // }
-// function setCurrentsize(newsize) {
+
+// function setCurrentSize(newsize) {
 //     currentSize = newsize
 // }
 
+// /////////////////////////////////////////////////////////////
+// //*Query Selectors*//
+// //Grid Box
+// const gridBox = document.querySelector("#grid");
+// let sliderSlider = document.querySelector("#sizeSlider")
+// let sizeValue = document.querySelector("#sizeValue");
+
+// /////////////////////////////////////////////////////////////
+// //Color Selectors
+// let colorPicker = document.querySelector("#colorPicker");
+// const color = document.querySelector("#color");
+// const rainbow = document.querySelector("#rainbow");
+
+// //Reset
+// const eraser = document.querySelector("#eraser");
+// const reset = document.querySelector("#clear");
+
+// /////////////////////////////////////////////////////////////
+
+// colorPicker.oninput = (e) => setCurrentColor(e.target.value);
+// color.onclick = () => setCurrentMode('color');
+// rainbow.onclick = () => setCurrentMode('rainbow');
+// eraser.onclick = () => setCurrentMode('eraser');
+// clear.onclick = () => reloadGrid()
+// sizeSlider.onmousemove = (e) => updateSizeValue(e.target.value);
+// sizeSlider.onchange = (e) => changeSize(e.target.value);
+
+// let mouseDown = false;
+// document.body.onmousedown = () => (mouseDown = true);
+// document.body.onmouseup = () => (mouseDown = false);
+
 // function changeSize(value) {
-//     setCurrentsize(value)
-//     updateSizeValue(value)
+//     setCurrentSize(value);
+//     updateSizeValue(value);
+//     reloadGrid()
+// };
+
+// function updateSizeValue(value) {
+//     sizeValue.textContent = `${value} x ${value}`;
 // }
 
-// document.body.addEventListener('mousedown', (this) => {
-//     this.mouse = true;
-// });
-
-// document.body.addEventListener('mouseup', (this) => {
-//     this.mouse = false;
-// });
-
-//Rainbow Button
-// function setCurrentMode(newMode) {
-//     currentMode = newMode
+// function reloadGrid() {
+//     clearGrid()
+//     gridSetUp(currentSize);
 // }
-// rainbow.onclick = () => setCurrentMode('rainbow')
 
+// function clearGrid() {
+//     gridBox.textContent = "";
+// }
+
+// /////////////////////////////////////////////////////////////
+// //*Default Grid*//
+// function gridSetUp(size) {
+//     for (let i = 0; i < size ** 2; i++) {
+//         let grid = document.createElement("div");
+//         grid.classList.add('square');
+//         grid.addEventListener('mousedown', changeColor);
+//         grid.addEventListener('mouseover', changeColor);
+//         gridBox.appendChild(grid);
+//     }
+//     gridBox.style.gridTemplateColumns = `repeat(${size}, auto)`;
+//     gridBox.style.gridTemplateRows = `repeat(${size}, auto)`;
+// };
+
+// gridSetUp(defaultSize);
+
+// ////////////////////////////////////////////////////////////
+
+// //Color button
 // function changeColor(e) {
 //     if (e.type === "mouseover" && !mouseDown) return
 //     if (currentMode === "rainbow") {
@@ -142,16 +211,27 @@ slider.oninput = function () {
 //         e.target.style.backgroundColor = currentColor
 //     } else if (currentMode === 'eraser') {
 //         e.target.style.backgroundColor = "white"
-//     }
-//     console.log(e);
-// }
+//     };
+// };
 
-//color-picker button
-// function setCurrentColor(newColor) {
-//     currentColor = newColor
-// }
-// colorPicker.oninput = (e) => setCurrentColor(e.target.value);
-// console.log(newColor);
-// function updateSizeValue(value) {
-//     output.innerHTML = `${value} x ${value}`
+// // function activateButton(newMode) {
+// //     if (currentMode === "rainbow") {
+// //         rainbow.classList.remove('active')
+// //     } else if (currentMode === 'color') {
+// //         color.classList.remove('active')
+// //     } else if (currentMode === 'eraser') {
+// //         eraser.classList.remove('active')
+// //     }
 
+// //     if (newMode === "rainbow") {
+// //         rainbow.classList.add('active')
+// //     } else if (newMode === 'color') {
+// //         color.classList.add('active')
+// //     } else if (newMode === 'eraser') {
+// //         eraser.classList.add('active')
+// //     }
+// // }
+
+// window.onload = () => {
+//     gridSetUp(defaultSize)
+// }
